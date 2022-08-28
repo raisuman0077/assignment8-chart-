d59c10b8f5a01a0cd1189f3d23204350b36d22a6
bipnrai054 <bipinrai054@gmail.com>
2022-08-28 15:41:05 +0545

    doughnut done

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Bubble, Doughnut, Bar } from 'react-chartjs-2';
 
-function App() {
-  const [count, setCount] = useState(0)
+ChartJS.register(ArcElement, Tooltip, Legend);
 
+export const data = {
+  labels: [
+    '2021-07-11',
+    '2021-07-12',
+    '2021-07-13',
+    '2021-07-14',
+    '2021-07-15',
+    '2021-07-16',
+    '2021-07-17',
+  ],
+  datasets: [
+    {
+      data: [53, 42, 88, 14, 77, 68, 43],
+      backgroundColor: [
+        'rgba(54, 65, 132, 0.2)',
+        'rgba(255, 65, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(200, 65, 132, 0.2)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+export default function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div
+      style={{
+        height: '500px',
+        width: '900px',
+      }}
+    >
+      <Doughnut data={data} />;
     </div>
-  )
+  );
 }
-
-export default App
